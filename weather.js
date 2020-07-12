5250a2bb34a5e5d68a56c6fa4dadfb039e269abf
JeongSikKim <JeongSikKim@bitbucket.org>
2020-07-12 14:55:11 +0900

    coordinate postion info

--- weather.js
+++ weather.js
@@ -3,12 +3,11 @@ const weather = document.querySelector(".js-weather");
 const API_KEY = `e9a5f7cfb860c80be9185edc4f77142d`;
 const COORDS = `coords`;
 
-function getWeather(lat,lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    )
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
         .then(function (response) {
-        return response.json();
-    })
+            return response.json();
+        })
         .then(function (json) {
             const temperature = json.main.temp;
             const place = json.name;
@@ -38,15 +37,15 @@ function handleGeoError() {
 }
 
 function askForCoords() {
-    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError)
-
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
+
 
 function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS);
-    if(loadedCoords === null){
+    if (loadedCoords === null) {
         askForCoords();
-    } else{
+    } else {
         const parsedCoords = JSON.parse(loadedCoords);
         console.log(parsedCoords);
         getWeather(parsedCoords.latitude, parsedCoords.longitude);
